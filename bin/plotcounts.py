e0ea2060e9049e7ec674faf93f7a88b920c6a972
adecora <alejandrodecora@gmail.com>
2023-03-07 19:37:59 +0100

    Plot frequency against rank on log-log axes

--- bin/plotcounts.py
+++ bin/plotcounts.py
@@ -11,7 +11,7 @@ def plotcounts(infile, outfile, xlim):
                                            method='max')
     df['inverse_rank'] = 1 / df['rank']
     scatplot = df.plot.scatter(x='word_frequency',
-                               y='inverse_rank',
+                               y='rank', loglog=True,
                                figsize=[12, 6],
                                grid=True,
                                xlim=args.xlim)
