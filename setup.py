12a64083a2f183d90df6312b80665a4289eb3d2c
adecora <alejandrodecora@gmail.com>
2023-04-10 16:41:40 +0200

    14 - creating packages with python

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
 
+with open('./README.rst', 'r') as reader:
+    long_description = reader.read()
+
 setup(
     name='pyzipf-cora',
-    version='0.1.0',
+    version='0.1.1',
     author='Alejandro de Cora',
+    description="Zipf's Law",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     packages=['pyzipf'],
     install_requires=[
         'matplotlib',
